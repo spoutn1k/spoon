5611090b7d18876222a01e75938e58c3429a4bc7
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2022-12-12 21:59:35 -0800

    Recipe markdown

--- src/app/recipe_window.rs
+++ src/app/recipe_window.rs
@@ -1,9 +1,80 @@
+use pulldown_cmark::{html::push_html, Options, Parser};
 use yew::prelude::*;
+use yew::{html, AttrValue, Html};
 
 #[derive(Properties, PartialEq, Clone)]
 pub struct RecipeWindowProps {
     pub url: String,
     pub recipe_id: Option<String>,
+}
+
+fn parse_text(value: &str) -> String {
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_TABLES);
+    options.insert(Options::ENABLE_FOOTNOTES);
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    options.insert(Options::ENABLE_TASKLISTS);
+
+    let parser = Parser::new_ext(&value, options);
+    let mut parsed_text = String::new();
+    push_html(&mut parsed_text, parser);
+
+    parsed_text
+}
+
+fn render_recipe(data: &ladle::models::Recipe) -> Html {
+    let data = data.clone();
+
+    let requirements = data
+        .requirements
+        .iter()
+        .map(|requirement| {
+            html! {
+                <li key={requirement.ingredient.id.clone()}>
+                    <span class="requirement-ingredient">{requirement.ingredient.name.clone()}</span>
+                    <span class="requirement-quantity">{requirement.quantity.clone()}</span>
+                </li>
+            }
+        })
+        .collect::<Html>();
+
+    let dependencies = data
+        .dependencies
+        .iter()
+        .map(|recipe| {
+            html! {
+                <li key={recipe.id.clone()}>
+                    {recipe.name.clone()}
+                </li>
+            }
+        })
+        .collect::<Html>();
+
+    let tags = data
+        .tags
+        .iter()
+        .map(|label| {
+            html! {
+                <li key={label.id.clone()}>
+                    {label.name.clone()}
+                </li>
+            }
+        })
+        .collect::<Html>();
+
+    let parse_html = parse_text(&data.directions);
+    let parsed = Html::from_html_unchecked(AttrValue::from(parse_html));
+
+    html! {
+        <div>
+            <div class="recipe-name">{data.name}</div>
+            <div class="recipe-author">{data.author}</div>
+            <ul>{dependencies}</ul>
+            <ul>{requirements}</ul>
+            <div class="recipe-directions">{parsed}</div>
+            <ul>{tags}</ul>
+        </div>
+    }
 }
 
 #[function_component(RecipeWindow)]
@@ -31,11 +102,7 @@ pub fn recipes_window(props: &RecipeWindowProps) -> Html {
     }
 
     match (*recipe).clone() {
-        Some(data) => html! {
-            <>
-                <div>{data.name}</div>
-            </>
-        },
+        Some(data) => render_recipe(&data),
         None => html! {<span>{"No data"}</span>},
     }
 }

97918e8a288bb00dac5e973dd6e53287f3736491
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2022-12-17 10:15:49 -0800

    Basic edition framework

--- src/app/recipe_edit.rs
+++ src/app/recipe_edit.rs
@@ -2,18 +2,68 @@ use crate::app::status_bar::Message;
 use std::ops::Deref;
 use yew::prelude::*;
 
-fn render_recipe(data: &ladle::models::Recipe, edit: Callback<bool>, delete: Callback<()>) -> Html {
-    let on_click_edit = Callback::from(move |_| edit.emit(false));
-    let on_click_delete = Callback::from(move |_| delete.emit(()));
+#[derive(Properties, PartialEq, Clone)]
+struct RequirementEditItemProps {
+    url: String,
+    requirement: ladle::models::Requirement,
+}
+
+#[function_component(RequirementEditItem)]
+fn requirement_edit_item(props: &RequirementEditItemProps) -> Html {
+    let quantity_buffer = use_state(|| props.requirement.quantity.clone());
+
+    /*let update_requirement = Callback::from(move |_| {
+        wasm_bindgen_futures::spawn_local(async move {
+            match ladle::
+        });
+    });*/
 
     html! {
-        <div class="recipe-display edit">
-            <div class="recipe-name">{data.name.as_str()}</div>
-            <div class="options">
-                <button onclick={on_click_edit}>{"Done"}</button>
-                <button onclick={on_click_delete}>{"Delete"}</button>
-            </div>
-        </div>
+        <li key={props.requirement.ingredient.id.as_str()}>
+            <span>{props.requirement.ingredient.name.as_str()}</span>
+            <input
+                type="text"
+                value={(*quantity_buffer).clone()}
+            />
+            <button>{"Update"}</button>
+            <button>{"Delete"}</button>
+        </li>
+    }
+}
+
+#[derive(Properties, PartialEq, Clone)]
+struct DependencyEditItemProps {
+    url: String,
+    recipe_id: String,
+    dependency: ladle::models::Dependency,
+    status: Callback<Message>,
+}
+
+#[function_component(DependencyEditItem)]
+fn dependency_edit_item(props: &DependencyEditItemProps) -> Html {
+    let props_cloned = props.clone();
+    let on_dependency_delete = Callback::from(move |_| {
+        let props_cloned = props_cloned.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            if let Err(message) = ladle::recipe_unlink(
+                props_cloned.url.as_str(),
+                props_cloned.recipe_id.as_str(),
+                props_cloned.dependency.id.as_str(),
+            )
+            .await
+            {
+                props_cloned
+                    .status
+                    .emit(Message::Error(message.to_string()));
+            }
+        });
+    });
+
+    html! {
+        <li key={props.dependency.id.as_str()}>
+            <span>{props.dependency.name.as_str()}</span>
+            <button onclick={on_dependency_delete}>{"Delete"}</button>
+        </li>
     }
 }
 
@@ -26,10 +76,12 @@ pub struct RecipeEditWindowProps {
     pub on_delete: Callback<()>,
 }
 
-#[derive(Properties, PartialEq, Clone)]
+#[derive(Clone)]
 pub struct RecipeEditWindowState {
     pub recipe: Option<ladle::models::Recipe>,
     pub name_buffer: String,
+    pub author_buffer: String,
+    pub directions_buffer: String,
 }
 
 #[function_component(RecipeEditWindow)]
@@ -37,6 +89,8 @@ pub fn recipe_edit_window(props: &RecipeEditWindowProps) -> Html {
     let state = use_state(|| RecipeEditWindowState {
         recipe: None,
         name_buffer: String::default(),
+        author_buffer: String::default(),
+        directions_buffer: String::default(),
     });
 
     let state_cloned = state.clone();
@@ -50,7 +104,12 @@ pub fn recipe_edit_window(props: &RecipeEditWindowProps) -> Html {
                     let mut data = state_cloned.deref().clone();
 
                     match ladle::recipe_get(props_cloned.url.as_str(), id.as_str()).await {
-                        Ok(recipe) => data.recipe = Some(recipe),
+                        Ok(recipe) => {
+                            data.name_buffer = recipe.name.clone();
+                            data.author_buffer = recipe.author.clone();
+                            data.directions_buffer = recipe.directions.clone();
+                            data.recipe = Some(recipe);
+                        }
                         Err(message) => props_cloned
                             .status
                             .emit(Message::Error(message.to_string())),
@@ -63,8 +122,73 @@ pub fn recipe_edit_window(props: &RecipeEditWindowProps) -> Html {
         props.clone(),
     );
 
-    match (*state).recipe.clone() {
-        Some(data) => render_recipe(&data, props.set_edition.clone(), props.on_delete.clone()),
-        None => html! {<span>{"No data"}</span>},
+    let state_cloned = state.clone();
+    let props_cloned = props.clone();
+    if let Some(recipe) = &(*state_cloned).recipe {
+        let on_click_edit = Callback::from(move |_| props_cloned.set_edition.emit(false));
+        let on_click_delete = Callback::from(move |_| props_cloned.on_delete.emit(()));
+
+        let dependencies = recipe
+            .dependencies
+            .iter()
+            .map(|d| {
+                html! {
+                    <DependencyEditItem
+                        url={props_cloned.url.clone()}
+                        recipe_id={recipe.id.clone()}
+                        dependency={d.clone()}
+                        status={props_cloned.status.clone()}
+                    />
+                }
+            })
+            .collect::<Html>();
+
+        let requirements = recipe
+            .requirements
+            .iter()
+            .map(|r| {
+                html! {
+                    <RequirementEditItem
+                        url={props_cloned.url.clone()}
+                        requirement={r.clone()}
+                    />
+                }
+            })
+            .collect::<Html>();
+
+        html! {
+            <div class="recipe-display edit">
+                <div>
+                    <input type="text"
+                        class="recipe-name edit"
+                        value={state_cloned.name_buffer.clone()}
+                    />
+                    <button>{"Update"}</button>
+                </div>
+                <div>
+                    <input type="text"
+                        class="recipe-author edit"
+                        value={state_cloned.author_buffer.clone()}
+                    />
+                    <button>{"Update"}</button>
+                </div>
+                <ul>
+                    {dependencies}
+                </ul>
+                <ul>
+                    {requirements}
+                </ul>
+                <textarea
+                    class="recipe-directions edit"
+                    value={state_cloned.directions_buffer.clone()}
+                />
+                <div class="options">
+                    <button onclick={on_click_edit}>{"Done"}</button>
+                    <button onclick={on_click_delete}>{"Delete"}</button>
+                </div>
+            </div>
+        }
+    } else {
+        html! {}
     }
 }
